5f08e30e6710b533c33b6dcbb7055433169548ff
baptiste-mouton <baptiste.mouton@etu.umontpellier.fr>
2020-03-07 12:45:32 +0100

    DB config changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,10 @@
 let mysql = require('mysql');
 
 var mysqlConnection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "baptisteMoutonsql08",
-    database: "projetAuxVie",
+    host: "eu-cdbr-west-02.cleardb.net",
+    user: "b7027415a26b90",
+    password: "fe28dccd",
+    database: "heroku_b22850cda3eabc3",
     multipleStatements: true
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -5,66 +5,47 @@ let cookie = require('cookie');
 let Intervention = require('../models/Intervention');
 let Creneau = require('../models/Creneau');
 
-    //ROUTES : 
+//ROUTES : 
 //Admin's Home:
-router.get('/',jwtUtils.authentification,jwtUtils.isAdmin, (req,res) => {
+router.get('/', jwtUtils.authentification, jwtUtils.isAdmin, (req, res) => {
     let User = require('../models/User');
     var userId = req.user.userId;
     User.getProfileInfos(userId, (user) => {
-        if(user[0] != undefined){
-            res.status(201).render('./privateRoutes/adminsHome',{'user':user[0]});
+        if (user[0] != undefined) {
+            res.status(201).render('./privateRoutes/adminsHome', { 'user': user[0] });
         } else {
-            res.status(404).render('index',{'error': 'Utilisateur invalide.'});
+            res.status(404).render('index', { 'error': 'Utilisateur invalide.' });
         }
+    })
 })
-})
 
-router.get('/profil', jwtUtils.authentification, (req,res) => {
-    /*var headerAuth = req.headers['authorization'];
-    console.log(headerAuth)
-    var userId = jwtUtils.getUserId(headerAuth);
-
-    if(userId<0){
-        return res.status(400).json({'error': 'Wrong token!'});
-    }
-    
-    let User = require('../models/User');
-    User.getProfileInfos(userId, (user) => {
-        if(user[0] != undefined){
-            res.status(201).json(user[0]);
-            res.render('/profil',{user:user[0]});
-        } else {
-            res.status(404).json({'error': 'user not found'});
-        }
-    })*/
-
+router.get('/profil', jwtUtils.authentification, (req, res) => {
     let User = require('../models/User');
     var userId = req.user.userId;
     User.getProfileInfos(userId, (user) => {
-        if(user[0] != undefined){
-            res.status(201).render('./privateRoutes/profil',{'user':user[0]});
+        if (user[0] != undefined) {
+            res.status(201).render('./privateRoutes/profil', { 'user': user[0] });
         } else {
-            res.status(404).json({'error': 'user not found'});
+            res.status(404).json({ 'error': 'user not found' });
         }
-})
+    })
 });
 
-router.get('/reservations',jwtUtils.authentification, (req,res) => {
-    //get toutes les réservations dont la date est > ajd
+router.get('/reservations', jwtUtils.authentification, (req, res) => {
     Intervention.getInterToComeByClient(req.user.userId, (interventions) => {
         console.log(interventions);
         let results = [];
-        for(intervention of interventions){
+        for (intervention of interventions) {
             let creneauI = Creneau.StringFromId(intervention.Creneau_idCreneau);
-            results.push("'" + intervention.libelleIntervention + "'" + " le " + intervention.dateIntervention.toISOString().slice(0,10) + " sur le créneau " + creneauI);
+            results.push("'" + intervention.libelleIntervention + "'" + " le " + intervention.dateIntervention.toISOString().slice(0, 10) + " sur le créneau " + creneauI);
         }
-        res.status(201).render('./privateRoutes/clientsReservations',{'interventions': results});
+        res.status(201).render('./privateRoutes/clientsReservations', { 'interventions': results });
     })
 })
 
 //DISCONNECT : 
-router.get('/disconnect',jwtUtils.authentification, (req,res) => {
-    res.setHeader('Set-Cookie',cookie.serialize('authToken', ''));
+router.get('/disconnect', jwtUtils.authentification, (req, res) => {
+    res.setHeader('Set-Cookie', cookie.serialize('authToken', ''));
     console.log("You are logged off");
     return res.status(302).redirect('/');
 })
